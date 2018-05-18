f287c54021563d308a4fe011557688be66e88d0f
IamPao <pao012345@hotmail.com>
2018-05-18 14:03:14 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,12 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'G8K/xCgYaaQQz7KfFFNhtr74/xoYUL9qErsYgIc8RJuN+ZsDOt6E4AoB8I0co3Uli8nb2bN9FkizTEwQshXJcBXPq5li8H+76SjxhIvZNc1z7aiUXQ0twn4Ba8okJV6MCPelfG9r7zBMJHKcAh1JMwdB04t89/1O/w1cDnyilFU=
+';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '1b621aa964edc8ffba41470c8dc55119';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'Uc581721d958bb121a98ca0979e2b27c3';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
