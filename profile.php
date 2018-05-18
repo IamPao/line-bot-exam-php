28dff00c342cf488ef35292e41c4f75e2cf6bb68
IamPao <pao012345@hotmail.com>
2018-05-18 14:23:34 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'G8K/xCgYaaQQz7KfFFNhtr74/xoYUL9qErsYgIc8RJuN+ZsDOt6E4AoB8I0co3Uli8nb2bN9FkizTEwQshXJcBXPq5li8H+76SjxhIvZNc1z7aiUXQ0twn4Ba8okJV6MCPelfG9r7zBMJHKcAh1JMwdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Uc581721d958bb121a98ca0979e2b27c3';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
